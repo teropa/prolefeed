5f4b9d33e1f09465ba382e47c1b7414f9f10f938
Tero Parviainen <tero.parviainen@eficode.fi>
2010-05-13 11:08:40 +0300

    Added Clojars support to build file

--- project.clj
+++ project.clj
@@ -2,5 +2,8 @@
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
                  [net.java.dev.rome/rome-fetcher "1.0.0"]]
-  :namespaces [prolefeed])
+  :dev-dependencies [[lein-clojars "0.5.0"]]
+  :namespaces [prolefeed]
+  :description "A simple Clojure library for fetching RSS/Atom feeds"
+  :url "http://github.com/teropa/prolefeed")
 
